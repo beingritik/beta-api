72640a788514958623c21f8189ad532c20724e26
beingritik <ritikjain98.r@gmail.com>
2023-03-03 11:50:48 +0530

    login issue

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 require("express-async-errors");
 const express = require("express");
 const app = express();
-const LoginRouter = require("./routes/Loginrouter");
+const LoginRouter = require("./routes/loginRouter");
 const adminRouter = require("./routes/adminRouter");
 const studentRouter = require("./routes/studentRouter");
 const notFoundMiddleware = require('./middleware/notFound');
--- routes/LoginRouter.js
+++ routes/LoginRouter.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const adminRouter = express.Router();
+const loginRouter = express.Router();
 
 //Required controllers for user
 const authControllerVar = require("../controllers/auth");
 
-adminRouter
+loginRouter
 .post("/adminlogin", authControllerVar.adminLogin)
 .post("/userlogin", authControllerVar.userLogin)
 
-module.exports = adminRouter;
+module.exports = loginRouter;
