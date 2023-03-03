9a3aa1fe04f6048d7bb04195d5fbcf48bc2df0ab
beingritik <ritikjain98.r@gmail.com>
2023-03-03 12:40:27 +0530

    router commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 require("express-async-errors");
 const express = require("express");
 const app = express();
-const loginRouter = require("./routes/loginRouter"); 
+const loginRouter = require("./routes/router"); 
 const adminRouter = require("./routes/adminRouter");
 const studentRouter = require("./routes/studentRouter");
 const notFoundMiddleware = require('./middleware/notFound');
